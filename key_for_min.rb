1d9f8c675c4c3ae4f3090451384c36247a2ec3fd
Jane Venter <janeanneventer@gmail.com>
2018-10-20 04:11:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
-  name_hash.each_pair do |key, value|
+  name_hash.each do |key, value|
      if value < min_value 
        min_value = value
        min_key = key
